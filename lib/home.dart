a778ae9a949223f78f810659ba3760d1d7415133
LeaOhyeah <leasehat@gmail.com>
2025-02-14 22:20:38 +0700

    feat(home) menambahkan contoh container di home
    membungkus teks dengan container dan melakukan styling seperti mengatur box (padding, margin, align) serta panjang dan tinggi container

--- lib/home.dart
+++ lib/home.dart
@@ -6,26 +6,34 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        leading: Icon(Icons.menu),
-        titleSpacing: 0,
-        centerTitle: true,
-        title: Text("Halo Mobile"),
-        actions: [
-          Icon(Icons.notifications),
-          Icon(Icons.settings),
-          
-        ],
-        backgroundColor: Colors.blue,
-      ),
-      body: const Center(
-        child: Icon(
-          Icons.face,
-          color: Colors.blue,
-          size: 48,
+        appBar: AppBar(
+          leading: Icon(Icons.menu),
+          titleSpacing: 0,
+          centerTitle: true,
+          title: Text("Halo Mobile"),
+          actions: [
+            Icon(Icons.notifications),
+            Icon(Icons.settings),
+          ],
+          backgroundColor: Colors.blue,
         ),
-      ),
-      // body: Center(child: Text("Ini Home dari file lain"),),
-    );
+        body: Container(
+          height: 100,
+          width: 300,
+          margin: EdgeInsets.all(30),
+          alignment: Alignment.center,
+          // padding: EdgeInsets.all(16),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(30.0),
+            color: Colors.amber,
+          ),
+          // color: Colors.amber,
+          child: Text(
+            "Text container",
+            style: TextStyle(fontSize: 30),
+          ),
+        )
+        // body: Center(child: Text("Ini Home dari file lain"),),
+        );
   }
 }
